482e1ad39f37726dfc9087ca23d3234279e4bb19
Chiranjibi Karki <ckarki70@gmail.com>
2023-07-06 12:20:10 -0400

    comments added

--- Desktop/Ts basics/index.js
+++ Desktop/Ts basics/index.js
@@ -1,23 +1,32 @@
+//function that has str as parameter
 function firstNonRepeated(str) {
+    //first we split each letter of incoming str into separate array
     var sArray = [];
     sArray = str.split("");
+    //we have finalArr array to store non repeated letters
     var finalArr = [];
     var count = 0;
+    //looping through incoming string str and letter splitted array sArray
     for (var _i = 0, str_1 = str; _i < str_1.length; _i++) {
         var s = str_1[_i];
         for (var _a = 0, sArray_1 = sArray; _a < sArray_1.length; _a++) {
             var c = sArray_1[_a];
+            //if the letter matched then count is increased
             if (s === c) {
                 count += 1;
             }
         }
+        //if count is not greater than one means it is not repeated then we store in finalArr
         if (count <= 1) {
             finalArr.push(s);
         }
+        //setting count to 0 again to move in next loop
         count = 0;
     }
+    //if array length is greater than 0 then we return first non repeated value else we return empty string "" 
     if (finalArr.length > 0)
         return finalArr[0];
     return "";
 }
+//calling and passing the argument to the function
 console.log(firstNonRepeated("aabcc"));
--- Desktop/Ts basics/index.ts
+++ Desktop/Ts basics/index.ts
@@ -1,24 +1,34 @@
+//function that has str as parameter
 function firstNonRepeated(str:string):string{
+
+    //first we split each letter of incoming str into separate array
     let sArray:string[]=[]
     sArray=str.split("");
 
+    //we have finalArr array to store non repeated letters
     let finalArr:string[]=[];
     let count:number=0;
 
+    //looping through incoming string str and letter splitted array sArray
     for(let s of str){
         for(let c of sArray){
+            //if the letter matched then count is increased
             if(s===c){
                 count+=1;
             }
         }
+        //if count is not greater than one means it is not repeated then we store in finalArr
         if(count<=1){
             finalArr.push(s);
         }
+        //setting count to 0 again to move in next loop
         count=0;
     }
 
+    //if array length is greater than 0 then we return first non repeated value else we return empty string "" 
     if(finalArr.length>0) return finalArr[0];
     return "";
 
 }
+//calling and passing the argument to the function
 console.log(firstNonRepeated("aabcc"));
